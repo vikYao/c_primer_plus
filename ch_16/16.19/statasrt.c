22968f38096404b7bcbf24ea3772410d3e62ecae
XuanYaoWei <1577535818@qq.com>
2019-08-26 13:11:56 +0000

    Modify code : 16.19

--- ch_16/16.19/statasrt.c
+++ ch_16/16.19/statasrt.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <limits.h>
-_Static_assert(CHAR_BIT == 16, "16-bit char falsely assumed");
+_Static_assert(CHAR_BIT == 16, "16-bit char falsely assumed");  // 编译时检查表达式，无法通过编译
 int main(void)
 {
 	puts("char is 16 bits.");
